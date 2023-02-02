3937edd5475c27d9ace4aac23261c0f808d65eb4
Vesku2000 <valtterisyrjanen@gmail.com>
2023-02-02 08:48:41 +0200

    task24 done

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
+//import './Button.css';
 
+
+const Button = styled.button`
+
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #F2CD5C;
+  color: #F2CD5C;
+  background: #400E32;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+
+&:focus {
+  outline: none;
+}
+
+&:hover,
+&:active {
+  background: #A61F69;
+  border-color: #ac0e77;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+`;
+/*
 const Button = props => {
   return (
     <button type={props.type} className="button" onClick={props.onClick}>
@@ -9,5 +34,7 @@ const Button = props => {
     </button>
   );
 };
+
+*/
 
 export default Button;
