a7aba549c15203c5d230ce7adc6333ff22ba5d95
Vesku2000 <valtterisyrjanen@gmail.com>
2023-02-02 09:10:22 +0200

    task 25 done

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 
 const Button = styled.button`
-
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #F2CD5C;
@@ -13,6 +13,10 @@ const Button = styled.button`
   background: #400E32;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px){
+    width: auto;
+  }
 
 
 &:focus {
